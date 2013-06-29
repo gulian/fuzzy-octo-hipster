46bdc938e98eb3821b2c6678bab49f7bdf0766fc
gulian <gulian@orange.fr>
2013-06-29 11:40:51 +0200

    clean code

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,26 +1,29 @@
 angular.module('drunkenbear', []).config(function($interpolateProvider) {
+
 	$interpolateProvider.startSymbol('[[');
 	$interpolateProvider.endSymbol(']]');
+
 }).config(['$routeProvider', function($routeProvider) {
 
 	$routeProvider.
-			when('/movies',				{templateUrl: 'partials/movie-list.html'	, controller: moviesListController}).
-			when('/movies/:movieId',	{templateUrl: 'partials/movie-details.html'	, controller: moviesDetailController}).
-			otherwise({redirectTo: '/movies'});
+		when('/movies',				{templateUrl: 'partials/movie-list.html'	, controller: moviesListController}).
+		when('/movies/search/:query',	{templateUrl: 'partials/movie-list.html'	, controller: moviesListController}).
+		when('/movies/:movieId',	{templateUrl: 'partials/movie-details.html'	, controller: moviesDetailController}).
+		otherwise({redirectTo: '/movies'});
 
 }]);
 
 
-function moviesListController($scope, $http) {
+function moviesListController($scope, $routeParams, $http) {
 
 	$scope.orderProp = 'title';
+	$scope.query = $routeParams.query;
 
 	$http.get('item/').success(function(data) {
 		$scope.movies = data;
 	});
 
 	$scope.details = function(){
-		console.log('detail');
 	};
 
 }
@@ -33,7 +36,7 @@ function moviesDetailController($scope, $routeParams, $http) {
 		$scope.movie = data;
 	});
 
-	$scope.share = function() {
+	$scope.buy = function() {
 		window.open($scope.movie.amazon_url,'_blank');
 	};
 
