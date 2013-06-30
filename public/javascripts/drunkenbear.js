21fa5a64f9698b119a6ce0d349af44a129c462d9
gulian <gulian@orange.fr>
2013-06-30 16:47:38 +0200

    add typeahead example for @jc950

--- public/javascripts/drunkenbear.js
+++ public/javascripts/drunkenbear.js
@@ -1,5 +1,4 @@
-angular.module('drunkenbear', []).config(function($interpolateProvider) {
-
+angular.module('drunkenbear', ['ui.bootstrap']).config(function($interpolateProvider) {
 	$interpolateProvider.startSymbol('[[');
 	$interpolateProvider.endSymbol(']]');
 
@@ -22,6 +21,12 @@ function moviesListController($scope, $routeParams, $http) {
 		$scope.movies = data;
 	});
 
+}
+
+function TypeaheadCtrl($scope) {
+
+  $scope.selected = undefined;
+  $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 }
 
 function moviesDetailController($scope, $routeParams, $http, $location) {
